12026496930f35fcd6e14fa7e443660930574da6
avangogo <avangogo@hotmail.com>
2012-12-02 04:46:22 +0100

    I just added the license text.

--- src/ecriture/main.ml
+++ src/ecriture/main.ml
@@ -1,3 +1,21 @@
+(* 
+   Lettreries is a random poem generator.
+    Copyright (C) 2012 Rémi de Verclos
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*)
+
 open Grammaire
 (*module DRime = Debug.OfOrderConstraint (Rime)
 module DPieds = Debug.OfMetricConstraint (Pieds)
