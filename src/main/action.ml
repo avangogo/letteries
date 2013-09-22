dc296e8174ecc119770ee46c11686fa223940f9b
avangogo <avangogo@hotmail.com>
2013-09-22 20:33:30 +0200

    Centralization of some operations in the Action module

---
+++ src/main/action.ml
@@ -0,0 +1,11 @@
+let loadPhonetic () = 
+  let rules = !Param.phoneticrules_file
+  and target = !Param.phoneticautomaton_file in
+  Make.load
+    ~makeMessage:"Précalcul des règles phonétiques…"
+    ~loadMessage:"Chargement des Règles de phonétique…"
+    [rules] target (fun () -> Phonetique.make_automate rules);;
+
+let makeClean () =
+  Sys.remove !Param.phoneticautomaton_file;
+  Sys.remove !Param.grammarautomaton_file;;
